e5bac3474dbc9cc5df3ca27d7d6d66c2cf46c5d6
Emad <diyanat00emad@gmail.com>
2023-10-22 23:12:16 +0330

    add int varible

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -11,42 +11,12 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            /*XmlWriter xmlWriter = XmlWriter.Create("Nasra.xml");
-            xmlWriter.WriteStartDocument();
-            xmlWriter.WriteStartElement("User");
 
-            xmlWriter.WriteStartElement("Member");
-            xmlWriter.WriteAttributeString("Age", "20");
-            xmlWriter.WriteString("ali");
-            xmlWriter.WriteEndElement();
-
-            xmlWriter.WriteStartElement("Member1");
-            xmlWriter.WriteAttributeString("Age", "23");
-            xmlWriter.WriteString("emad");
-            xmlWriter.WriteEndElement();
-
-
-
-            xmlWriter.WriteEndDocument();
-            xmlWriter.Close();*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            int alli = 110;
 
         }
+
     }
 }
+    
+
