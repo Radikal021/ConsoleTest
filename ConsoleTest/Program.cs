4720764aaddcfbcaf873ec974699c47691381d9e
Emad <diyanat00emad@gmail.com>
2023-10-22 22:14:17 +0330

    Add project files.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*XmlWriter xmlWriter = XmlWriter.Create("Nasra.xml");
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("User");
+
+            xmlWriter.WriteStartElement("Member");
+            xmlWriter.WriteAttributeString("Age", "20");
+            xmlWriter.WriteString("ali");
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("Member1");
+            xmlWriter.WriteAttributeString("Age", "23");
+            xmlWriter.WriteString("emad");
+            xmlWriter.WriteEndElement();
+
+
+
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
